b749cc8cc72db90cfef5ca0ecda6f67195a97fe4
Jedrzej Lagodzinski <jedrzejszymon@gmail.com>
2021-05-04 22:09:53 +0200

    adding logic for routing categories

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,11 +1,16 @@
-import React from 'react';
+import React from "react";
+import { Route } from "react-router-dom";
 
-import CollectionsOverview from '../../components/collections-overview/collections-overview.component';
+import CollectionPage from '../collection/collection.component';
+import CollectionsOverview from "../../components/collections-overview/collections-overview.component";
 
-const ShopPage = ({ collections }) => (
-    <div className='shop-page'>
-      <CollectionsOverview />
+const ShopPage = ({ match }) => {
+  return (
+    <div className="shop-page">
+      <Route exact path={`${match.path}`} component={CollectionsOverview} />
+      <Route path={`${match.path}/:categoryId`} component={CollectionPage} />
     </div>
-);
+  );
+};
 
 export default ShopPage;
